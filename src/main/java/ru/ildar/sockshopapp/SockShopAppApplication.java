95048c194936dce6cfb90193c7baf231fba5cbc5
Home <ildar86@live.ru>
2023-01-25 12:27:30 +0500

    first commit

---
+++ src/main/java/ru/ildar/sockshopapp/SockShopAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ildar.sockshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockShopAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ildar/sockshopapp/SockShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ildar.sockshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
